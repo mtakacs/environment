ca4a3ce3695caa000bd22211e906bc5e3b4d835e
Mark Takacs <mtakacs@apple.com>
2021-02-12 09:48:46 -0800

    updates Feb 2021

--- s/git_config_email.sh
+++ s/git_config_email.sh
@@ -2,12 +2,16 @@
 
 for dir in *; do
     if [ -d "${dir}" ]; then
-        echo "${dir}: setting git config options"
+        echo "REPO: ${dir}: Running commands.."
         cd ${dir} 
         git config user.name "Mark Takacs"
         git config user.email "mtakacs@apple.com" 
-        git config user.signingkey 066E2D2C
-        git config push.followTags true
+        #git config push.followTags true
+        #git checkout 1.45
+        git pull
+        # git fetch
         cd ..
+        echo "REPO: ${dir}: done"
+        echo 
     fi
 done
